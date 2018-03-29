f5aedd551c5a898b578e7c5d4c575aa0250a3536
Giordon Stark <kratsg@gmail.com>
2018-03-29 10:52:44 -0500

    update gfex_power to use periphery

--- gfex_power.py
+++ gfex_power.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 from __future__ import print_function
-from i2cdev import I2C
 from math import trunc
 from time import sleep
 import sys
 
+from periphery import I2C
 from constants import *
+
+v4a = True
+v4b = False
 
 #convert a LinearFloat5_11 formatted word into a floating point value
 def lin5_11ToFloat(wordValue):
@@ -26,39 +29,33 @@ def lin5_11ToFloat(wordValue):
   # compute value as
   return mantissa * (2**exponent)
 
-from smbus import SMBusWrapper, i2c_msg
 def bmr458_mon(dev_addr,reg_addr):
-  with SMBusWrapper(1) as bus:
-    write = i2c_msg.write(dev_addr, [reg_addr])
-    read = i2c_msg.read(dev_addr, 2)
-    bus.i2c_rdwr(write, read)
-    #sleep(1)
-  value = list(read)
-  #print("[{0}]".format(', '.join(map(str, value))))
-  value = value[1]*256 + value[0]
-  #print('BMR458 value  is {0:d}' .format(value))
-  return '{0:x}'.format(value)
+  i2c = I2C("/dev/i2c-1")
+  write = I2C.Message([reg_addr])
+  read = I2C.Message([0x0]*2, read=True)
+  i2c.transfer(dev_addr, [write, read])
+  i2c.close()
+  return bytes(bytearray(read.data)).encode('hex')
 
 def ina226_reg_write(dev_addr,reg_addr,reg_value0,reg_value1):
-  i2c = I2C(dev_addr, 1) # device @ dev_addr, bus 1
-  i2c.write(bytearray([reg_addr,reg_value0,reg_value1]))# SENSOR_IIC_BUS is selected
-  #print('write done')
+  i2c = I2C("/dev/i2c-1")
+  i2c.transfer(dev_addr, [I2C.Message([reg_addr,reg_value0,reg_value1])])
   i2c.close()
+
 def ina226_reg_read(dev_addr,reg_addr,nbits):
-  i2c = I2C(dev_addr, 1) # device @ dev_addr, bus 1
-  i2c.write(bytearray([reg_addr]))# Reg for read
-  reg_value=i2c.read(nbits)# Read N bits.
+  i2c = I2C("/dev/i2c-1")
+  read = I2C.Message([0x0]*nbits, read=True)
+  i2c.transfer(dev_addr, [I2C.Message([reg_addr]),read])
   i2c.close()
-  #print('read done')
-  return reg_value
+  return bytes(bytearray(read.data)).encode('hex')
 
 def ltc2499_current_mon(dev_addr,reg_addr0,reg_addr1):
-  i2c = I2C(dev_addr, 1) # device @ dev_addr, bus 1
-  i2c.write(bytearray([reg_addr1,reg_addr0]))# Reg for read
+  i2c = I2C("/dev/i2c-1")
+  i2c.transfer(dev_addr, [I2C.Message([reg_addr1, reg_addr0])])
   sleep(0.5)
-  adc_code=i2c.read(4)# Read 4 bits.
+  i2c.transfer(dev_addr, [I2C.Message([0x0]*4, read=True)])
+  adc_code=bytes(bytearray(read.data)).encode('hex')
   i2c.close()
-  sleep(0.0)
   resolution=2500./0x80000000
   if(int(adc_code,16)<0x40000000):
     amplitude=0
@@ -67,34 +64,42 @@ def ltc2499_current_mon(dev_addr,reg_addr0,reg_addr1):
   return amplitude/40
 
 def adm1066_voltage_mon(dev_addr,reg_value_80,reg_value_81,reg_addr_vh,reg_addr_vl,average_on):
+  i2c = I2C("/dev/i2c-1")
   #read the ADM1066 U52 with addr 0x34 and 0x35
-  i2c = I2C(dev_addr, 1) # device 0x34/0x35
-  i2c.write(bytearray([0x82,0x0+average_on*4]))# reset STOPWRITE BIT
-  i2c.write(bytearray([0x80,reg_value_80]))# reg 0x80 select channel
-  i2c.write(bytearray([0x81,reg_value_81]))# reg 0x81
-
-  i2c.write(bytearray([0x82,0x01+average_on*4]))# reg 0x82 select go bit
-  i2c.write(bytearray([0x82]))# reg 0x82
-  status=i2c.read(1)# read the go bit status
+  read = I2C.Message([0x0], read=True)
+  i2c.transfer(dev_addr, [I2C.Message([0x82,0x0+average_on*4]), # reset STOPWRITE BIT
+                          I2C.Message([0x80,reg_value_80]),     # reg 0x80 select channel
+                          I2C.Message([0x81,reg_value_81]),     # reg 0x81
+                          I2C.Message([0x82,0x01+average_on*4]),# reg 0x82 select go bit
+                          I2C.Message([0x82]),                  # reg 0x82
+                          read                                  # read the go bit status
+                          ])
+  status=read.data[0]
   if int(status,16) != average_on*4:
-    status=i2c.read(1) #keep checking the go bit, until it is equal average_on*4
+    i2c.transfer(dev_addr, [read])
+    status=read.data[0] #keep checking the go bit, until it is equal average_on*4
     sleep(0.5)
-  i2c.write(bytearray([0x82,0x08+average_on*4]))
 
-  i2c.write(bytearray([reg_addr_vh]))# reg 0xA8 VH high 8bit voltage value
-  vh=i2c.read(1)#
-  i2c.write(bytearray([reg_addr_vl]))# reg 0xA9 VH low 8 bits voltag value
-  vl=i2c.read(1)#
+  i2c.transfer(dev_addr, [I2C.Message([0x82,0x08+average_on*4]),
+                          I2C.Message([reg_addr_vh]), # reg 0xA8 VH high 8bit voltage value
+                          read
+                          ])
+  vh=read.data[0]
+
+  i2c.transfer(dev_addr, [I2C.Message([reg_addr_vl]), # reg 0xA9 VH low 8 bits voltag value
+                          read
+                          ])
+  vl=read.data[0]
   i2c.close()
+
   voltage= ((int(vh,16)*256+int(vl,16))*2.048/4095)/(2**(average_on*4))
   return voltage
-  #print('12V  measured by ADM1066 is {0:.3f} V'.format(voltage))
-
 
 #set the i2c mux channel, since all the device is under SENSOR_IIC_BUS, just need to set once at the begining.
-i2c = I2C(TCA9548_U93_ADDR, 1) # device @ 0x70, bus 1
-i2c.write(bytearray([SENSOR_IIC_BUS]))# SENSOR_IIC_BUS is selected
+i2c = I2C("/dev/i2c-1")
+i2c.transfer(TCA9548_U93_ADDR, [I2C.Message([SENSOR_IIC_BUS])]) # SENSOR_IIC_BUS is selected
 i2c.close()
+
 print('-------------Power monitoring of gFEX production board--------------------')
 print('Power Rail       Monitoring Device    Voltage(V)    Current(A)    Power(W)')
 #12V power module BMR458 Voltage
@@ -128,42 +133,6 @@ print('5V IPMI          ADM1066 U52          {0:.3f}         N/A           N/A' 
 voltage=adm1066_voltage_mon(ADM1066_U52_ADDR,0xFE,0x1F,0xA0,0xA1,1)# read ADM1066 U52 channel VP1 3.3V IPMI
 print('3.3V IPMI        ADM1066 U52          {0:.3f}         N/A           N/A' .format(voltage*4.363))
 
-voltage=adm1066_voltage_mon(ADM1066_U52_ADDR,0xFD,0x1F,0xA2,0xA3,1)# read ADM1066 U52 channel VP2 INT_A_0.85V
-current=ltc2499_current_mon(LTC2499_U1_ADDR,0x90,0xB0)#read current of INT_A_0.85V from LTC2499
-print('INT_A_0.85V      ADM1066 U52/LTC2499  {0:.3f}         {1:.3f}         {2:.3f}' .format(voltage,current,voltage*current))
-
-voltage=adm1066_voltage_mon(ADM1066_U52_ADDR,0xFB,0x1F,0xA4,0xA5,1)# read ADM1066 U52 channel VP3 MGTAVCC_A_0.9V
-current=ltc2499_current_mon(LTC2499_U1_ADDR,0x90,0xB8)##read current of MGTAVCC_A_0.9V from LTC2499
-print('MGTAVCC_A_0.9V   ADM1066 U52/LTC2499  {0:.3f}         {1:.3f}         {2:.3f}' .format(voltage,current,voltage*current))
-
-voltage=adm1066_voltage_mon(ADM1066_U52_ADDR,0xF7,0x1F,0xA6,0xA7,1)# read ADM1066 U52 channel VP4 MGTAVCC_A_1.2V
-current=ltc2499_current_mon(LTC2499_U1_ADDR,0x90,0xB1)##read current of MGTATT_A_1.2V from LTC2499
-print('MGTAVTT_A_1.2V   ADM1066 U52/LTC2499  {0:.3f}         {1:.3f}         {2:.3f}' .format(voltage,current,voltage*current))
-
-voltage=adm1066_voltage_mon(ADM1066_U52_ADDR,0xDF,0x1F,0xAA,0xAB,1)# read ADM1066 U52 channel VX1 INT_B_0.85V
-current=ltc2499_current_mon(LTC2499_U1_ADDR,0x90,0xB9)
-print('INT_B_0.85V      ADM1066 U52/LTC2499  {0:.3f}         {1:.3f}         {2:.3f}' .format(voltage,current,voltage*current))
-
-voltage=adm1066_voltage_mon(ADM1066_U52_ADDR,0xBF,0x1F,0xAC,0xAD,1)# read ADM1066 U52 channel VX2 MGTAVCC_B_0.9V
-current=ltc2499_current_mon(LTC2499_U1_ADDR,0x90,0xB2)
-print('MGTAVCC_B_0.9V   ADM1066 U52/LTC2499  {0:.3f}         {1:.3f}         {2:.3f}' .format(voltage,current,voltage*current))
-
-voltage=adm1066_voltage_mon(ADM1066_U52_ADDR,0x7F,0x1F,0xAE,0xAF,1)# read ADM1066 U52 channel VX3 MGTAVTT_B_1.2V
-current=ltc2499_current_mon(LTC2499_U1_ADDR,0x90,0xBA)
-print('MGTAVTT_B_1.2V   ADM1066 U52/LTC2499  {0:.3f}         {1:.3f}         {2:.3f}' .format(voltage,current,voltage*current))
-
-voltage=adm1066_voltage_mon(ADM1066_U52_ADDR,0xFF,0x1E,0xB0,0xB1,1)# read ADM1066 U52 channel VX4 INT_C_0.85V
-current=ltc2499_current_mon(LTC2499_U1_ADDR,0x90,0xB3)
-print('INT_C_0.85V      ADM1066 U52/LTC2499  {0:.3f}         {1:.3f}         {2:.3f}' .format(voltage,current,voltage*current))
-
-voltage=adm1066_voltage_mon(ADM1066_U52_ADDR,0x7F,0x1D,0xB2,0xB3,1)# read ADM1066 U52 channel VX5 MGTAVCC_C_0.9V
-current=ltc2499_current_mon(LTC2499_U1_ADDR,0x90,0xBB)
-print('MGTAVCC_C_0.9V   ADM1066 U52/LTC2499  {0:.3f}         {1:.3f}         {2:.3f}' .format(voltage,current,voltage*current))
-
-voltage=adm1066_voltage_mon(ADM1066_U51_ADDR,0xBF,0x1F,0xAC,0xAD,1)# read ADM1066 U51 channel VX2 MGTAVTT_C_1.2V
-current=ltc2499_current_mon(LTC2499_U1_ADDR,0x90,0xB4)
-print('MGTAVTT_C_1.2V   ADM1066 U51/LTC2499  {0:.3f}         {1:.3f}         {2:.3f}' .format(voltage,current,voltage*current))
-
 voltage=adm1066_voltage_mon(ADM1066_U51_ADDR,0x7F,0x1F,0xAE,0xAF,1)# read ADM1066 U51 channel VX3 INT_Z_0.85V
 current=ltc2499_current_mon(LTC2499_U1_ADDR,0x90,0xBC)
 print('INT_Z_0.85V      ADM1066 U51/LTC2499  {0:.3f}         {1:.3f}         {2:.3f}' .format(voltage,current,voltage*current))
@@ -193,3 +162,39 @@ current=ltc2499_current_mon(LTC2499_U1_ADDR,0x90,0xBF)/10
 print('DDR4_VTT_0.6V    ADM1066 U51/LTC2499  {0:.3f}         N/A           N/A' .format(voltage))
 print('DDR4_VDDQ_1.2V   ADM1066 U51/LTC2499  {0:.3f}         {1:.3f}         {2:.3f}' .format(voltage*2,current,2*voltage*current))
 
+if v4b:
+  voltage=adm1066_voltage_mon(ADM1066_U52_ADDR,0xFD,0x1F,0xA2,0xA3,1)# read ADM1066 U52 channel VP2 INT_A_0.85V
+  current=ltc2499_current_mon(LTC2499_U1_ADDR,0x90,0xB0)#read current of INT_A_0.85V from LTC2499
+  print('INT_A_0.85V      ADM1066 U52/LTC2499  {0:.3f}         {1:.3f}         {2:.3f}' .format(voltage,current,voltage*current))
+
+  voltage=adm1066_voltage_mon(ADM1066_U52_ADDR,0xFB,0x1F,0xA4,0xA5,1)# read ADM1066 U52 channel VP3 MGTAVCC_A_0.9V
+  current=ltc2499_current_mon(LTC2499_U1_ADDR,0x90,0xB8)##read current of MGTAVCC_A_0.9V from LTC2499
+  print('MGTAVCC_A_0.9V   ADM1066 U52/LTC2499  {0:.3f}         {1:.3f}         {2:.3f}' .format(voltage,current,voltage*current))
+
+  voltage=adm1066_voltage_mon(ADM1066_U52_ADDR,0xF7,0x1F,0xA6,0xA7,1)# read ADM1066 U52 channel VP4 MGTAVCC_A_1.2V
+  current=ltc2499_current_mon(LTC2499_U1_ADDR,0x90,0xB1)##read current of MGTATT_A_1.2V from LTC2499
+  print('MGTAVTT_A_1.2V   ADM1066 U52/LTC2499  {0:.3f}         {1:.3f}         {2:.3f}' .format(voltage,current,voltage*current))
+
+  voltage=adm1066_voltage_mon(ADM1066_U52_ADDR,0xDF,0x1F,0xAA,0xAB,1)# read ADM1066 U52 channel VX1 INT_B_0.85V
+  current=ltc2499_current_mon(LTC2499_U1_ADDR,0x90,0xB9)
+  print('INT_B_0.85V      ADM1066 U52/LTC2499  {0:.3f}         {1:.3f}         {2:.3f}' .format(voltage,current,voltage*current))
+
+  voltage=adm1066_voltage_mon(ADM1066_U52_ADDR,0xBF,0x1F,0xAC,0xAD,1)# read ADM1066 U52 channel VX2 MGTAVCC_B_0.9V
+  current=ltc2499_current_mon(LTC2499_U1_ADDR,0x90,0xB2)
+  print('MGTAVCC_B_0.9V   ADM1066 U52/LTC2499  {0:.3f}         {1:.3f}         {2:.3f}' .format(voltage,current,voltage*current))
+
+  voltage=adm1066_voltage_mon(ADM1066_U52_ADDR,0x7F,0x1F,0xAE,0xAF,1)# read ADM1066 U52 channel VX3 MGTAVTT_B_1.2V
+  current=ltc2499_current_mon(LTC2499_U1_ADDR,0x90,0xBA)
+  print('MGTAVTT_B_1.2V   ADM1066 U52/LTC2499  {0:.3f}         {1:.3f}         {2:.3f}' .format(voltage,current,voltage*current))
+
+  voltage=adm1066_voltage_mon(ADM1066_U52_ADDR,0xFF,0x1E,0xB0,0xB1,1)# read ADM1066 U52 channel VX4 INT_C_0.85V
+  current=ltc2499_current_mon(LTC2499_U1_ADDR,0x90,0xB3)
+  print('INT_C_0.85V      ADM1066 U52/LTC2499  {0:.3f}         {1:.3f}         {2:.3f}' .format(voltage,current,voltage*current))
+
+  voltage=adm1066_voltage_mon(ADM1066_U52_ADDR,0x7F,0x1D,0xB2,0xB3,1)# read ADM1066 U52 channel VX5 MGTAVCC_C_0.9V
+  current=ltc2499_current_mon(LTC2499_U1_ADDR,0x90,0xBB)
+  print('MGTAVCC_C_0.9V   ADM1066 U52/LTC2499  {0:.3f}         {1:.3f}         {2:.3f}' .format(voltage,current,voltage*current))
+
+  voltage=adm1066_voltage_mon(ADM1066_U51_ADDR,0xBF,0x1F,0xAC,0xAD,1)# read ADM1066 U51 channel VX2 MGTAVTT_C_1.2V
+  current=ltc2499_current_mon(LTC2499_U1_ADDR,0x90,0xB4)
+  print('MGTAVTT_C_1.2V   ADM1066 U51/LTC2499  {0:.3f}         {1:.3f}         {2:.3f}' .format(voltage,current,voltage*current))
