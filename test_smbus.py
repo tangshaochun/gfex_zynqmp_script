a0c5a9799a3c462ceb91e9f441b70dc8c3d132ac
Giordon Stark <kratsg@gmail.com>
2018-03-29 15:04:32 -0500

    clean up smbus and i2cdev

--- test_smbus.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-from smbus import SMBusWrapper
-with SMBusWrapper(0) as bus:
-  bus.write_byte_data(0x68,0x01, 0x0)
-  #bus.write_byte_data(0x68,0x03)
-  value = bus.read_byte_data(0x68, 0x02)
-  value |= bus.read_byte_data(0x68, 0x03) << 8
-print value == 0x5345
