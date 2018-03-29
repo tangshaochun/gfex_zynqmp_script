6fa4c8344689f79d4b34bc7354a83dd040860d71
Giordon Stark <kratsg@gmail.com>
2018-03-29 14:38:00 -0500

    fix up test_i2c

--- test_i2c.py
+++ test_i2c.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 from periphery import I2C
+
+from constants import *
+
 i2c = I2C("/dev/i2c-0")
 
-i2c.transfer(0x68, [I2C.Message([0x0003])])
-
-read = I2C.Message([0x0]*2, read=True)
-i2c.transfer(0x68, [read])
+read = I2C.Message([0x2]*2, read=True)
+i2c.transfer(SI5345_U137_ADDR, [I2C.Message([0x01, 0x0])])
+i2c.transfer(SI5345_U137_ADDR, [I2C.Message([0x02])])
+i2c.transfer(SI5345_U137_ADDR, [read])
 i2c.close()
 
 print bytes(bytearray(read.data)).encode('hex') == '4553'
