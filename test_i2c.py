f27bdec34c921cf185f8f57cae4f30c7fbff9a56
Giordon Stark <kratsg@gmail.com>
2018-03-29 14:28:27 -0500

    fix no rep start

--- test_i2c.py
+++ test_i2c.py
@@ -2,8 +2,10 @@
 from periphery import I2C
 i2c = I2C("/dev/i2c-0")
 
+i2c.transfer(0x68, [I2C.Message([0x0003])])
+
 read = I2C.Message([0x0]*2, read=True)
-i2c.transfer(0x68, [I2C.Message([0x0003]), read])
+i2c.transfer(0x68, [read])
 i2c.close()
 
 print bytes(bytearray(read.data)).encode('hex') == '4553'
