52996b007f11a4b4c86c44b2e587f400c9ed9449
Giordon Stark <kratsg@gmail.com>
2018-03-29 14:27:06 -0500

    migrate most others

--- test_i2c.py
+++ test_i2c.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
-from i2cdev import I2C
-i2c = I2C(0x68, 0) # device @ 0x68, bus 0
-i2c.write(bytearray(0x0003)) # register for base part number
-value = i2c.read(2)
-print value == '4553'
+from periphery import I2C
+i2c = I2C("/dev/i2c-0")
+
+read = I2C.Message([0x0]*2, read=True)
+i2c.transfer(0x68, [I2C.Message([0x0003]), read])
+i2c.close()
+
+print bytes(bytearray(data.read)).encode('hex') == '4553'
