7f85e8ac5e410c842ea9f8a94dfdaed92c10731a
Giordon Stark <kratsg@gmail.com>
2018-03-29 14:27:32 -0500

    flipped around

--- test_i2c.py
+++ test_i2c.py
@@ -6,4 +6,4 @@ read = I2C.Message([0x0]*2, read=True)
 i2c.transfer(0x68, [I2C.Message([0x0003]), read])
 i2c.close()
 
-print bytes(bytearray(data.read)).encode('hex') == '4553'
+print bytes(bytearray(read.data)).encode('hex') == '4553'
